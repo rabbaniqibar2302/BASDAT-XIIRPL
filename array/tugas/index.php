03084cc04ee6dd0de01faa9a3a420fd025c08bb3
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-11-14 11:25:25 +0700

    latihan array

---
+++ array/tugas/index.php
@@ -0,0 +1,44 @@
+<?php
+
+//Penggunaan Array menggunakan for dan foreach
+//For = int
+//For perulangan yang akan di ulang sesuai set yang kita atur
+
+//variabel angka memiliki nilai pertama = 0 dan diakhiri kurang atau sama dengan 10 , dihitung increment
+// for ($angka = 0; $angka <= 10; $angka++){
+//     echo "angkanya adalah : $angka <br>";
+// }
+
+//foreach = string , char
+//Foreach mengulang sesuai nilai yang mau diambil (key)
+// $warna = array("merah","hijau","biru","kuning");
+
+// foreach ($warna as $variasi ) {
+//     echo "berwarna : $variasi <br>";
+// }
+
+// Tugas Menerapkan For dan Foreach menggunakan PHP
+$days = ["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"];
+?>
+
+<html>
+    <head>
+        <title>Latihan menggunakan for dan foreach</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
+        for ($hari = 0; $hari < count($days); $hari++){
+            ?>
+        <div class="box">
+            <?php echo $days[$hari];?>
+        </div>
+        <?php } ?>
+        <div class="clear"></div>
+        <?php foreach($days as $hari_hari) { ?>
+            <div class="box2">
+                <?php echo $hari_hari;?>
+            </div>
+        <?php } ?>
+    </body>
+</html>
