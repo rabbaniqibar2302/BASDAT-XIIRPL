1014e88887afec5e67029cb5727ab29103da0a85
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2022-11-22 13:40:33 +0700

    passing error to url

--- validasi/proses.php
+++ validasi/proses.php
@@ -9,12 +9,26 @@ $rules = [
     'email' => ['required','email'],
     'username' => ['required','username'],
     'usia' => ['required','numeric']
-];
+];  
 
 $errors = validasi($rules);
 
-echo "<pre>";
-print_r($errors);
-echo "</pre>";
+// echo "<pre>";
+// print_r($errors);
+// echo "</pre>";
+
+// Disini kita bisa melakukan proses yang harus dilakukan
+// jika tidak terjadi error validasi apapun
+
+if (count($errors) > 0){
+    $time = $_REQUEST;
+    $queryString = http_build_query([
+        'errors' => $errors,
+        'time' => $time
+    ]);
+
+    header("location: form.php?{$queryString}");
+    die();
+}
 
 ?>
